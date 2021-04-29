9d522c8d9686e4b952d3730b8656687b9672367a
MarshXy <57112423+Xy2002@users.noreply.github.com>
2021-04-29 20:49:01 +0800

    默认的

--- src/pages/personal/index.vue
+++ src/pages/personal/index.vue
@@ -1,16 +1,62 @@
 <template>
   <div :style="{minHeight: windowHeight+'px', minWidth: windowWidth+'px'}" class="wrapper">
 
-    <div v-if="isLogin">
+      <div v-if="isLogin">
+        <div class="bg-header">
+          <image class="bg_ware" src="https://cdn.nlark.com/yuque/0/2019/png/280373/1552908032971-assets/web-upload/e795fee4-c6cd-4951-b7d8-134aa9cd7c6b.png"></image>
+          <div class="userAvatar" style="padding-left: 5vw;padding-top: 5vh">
             <van-image
-            round
-            :src="userInfo.avatarUrl"
-            height="5rem"
-            width="5rem"
-          />
-          <p>{{userInfo.nickName}}</p>
-
+              :src="userInfo.avatarUrl"
+              height="5rem"
+              round
+              width="5rem"
+            />
+          </div>
+          <p style="position:absolute;padding-left: 28vw;color: white;bottom: 23vh;font-size: 20px">{{userInfo.nickName}}</p>
+        </div>
+        <div class="vanGrid">
+          <van-grid column-num="3">
+            <van-grid-item icon="medal-o" icon-color="#0048FFFF" text="赛事管理" />
+            <van-grid-item icon="manager-o" icon-color="#FF8C00FF" text="我的赛事" />
+            <van-grid-item icon="info-o" icon-color="#FF5100FF" text="个人详细" />
+          </van-grid>
+        </div>
+        <div class="pageBody">
+          <div class="realBody">
+            <van-cell-group>
+              <van-cell
+                is-link
+                link-type="switchTab"
+                size="large"
+                title="单元格"
+                url="/pages/index/main"
+              />
+              <van-cell
+                is-link
+                link-type="navigateTo"
+                size="large"
+                title="单元格"
+                url="/pages/counter/main"
+              />
+              <van-cell
+                is-link
+                link-type="navigateTo"
+                size="large"
+                title="单元格"
+                url="/pages/dashboard/index"
+              />
+              <van-cell
+                is-link
+                link-type="navigateTo"
+                size="large"
+                title="单元格"
+                url="/pages/dashboard/index"
+              />
+            </van-cell-group>
+          </div>
+        </div>
       </div>
+
 
 
     <div v-else class="box">
@@ -22,6 +68,7 @@
   </div>
 </template>
 <script>
+
 export default {
   data(){
     return{
@@ -82,8 +129,14 @@ export default {
 <style lang="stylus" rel="stylesheet/stylus" scoped>
 @import "../../../static/stylus/mixin.styl"
 body
-  height:100%
+  background-color #fff
   .wrapper
+    .bg-header
+      height 35vh
+      position relative
+      .bg_ware
+        position: absolute
+        width: 100vw
     .box
       position absolute
       top 50%
@@ -97,10 +150,16 @@ body
         padding-bottom: 40rpx
         margin-bottom: 40rpx
         text-align: center
-    .vanRow
-      position absolute
-      top 20%
+    .pageBody
+      top 10%
+      background-color #f1f1f1
+      height 53vh
+      .realBody
+        position relative
+        background-color white
+        top 3vh
+        box-shadow white
 
-
-
+</style>
+<style scoped>
 </style>
